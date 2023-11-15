0acc9498ee5d39654d0be20fdb499cab7524760a
Delaney Green <delaney.rosa1@gmail.com>
2023-11-15 15:57:40 -0600

    added mobile responsive design and logout button

--- petrax-frontend/src/app/navbar/navbar.component.ts
+++ petrax-frontend/src/app/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { MatMenuModule } from '@angular/material/menu';
+import { LoginService } from '../log-in/login.service';
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -9,14 +12,27 @@ import { filter } from 'rxjs/operators';
 })
 export class NavbarComponent implements OnInit {
   onHomePage: boolean;
+  onAboutPage: boolean;
+  isMenuOpen = false;
 
-  constructor(private router: Router) {}
+  toggleMenu() {
+    this.isMenuOpen=!this.isMenuOpen;
+  }
+
+  constructor(private router: Router, private loginService: LoginService, private ngZone: NgZone) {}
 
   ngOnInit(): void {
  this.router.events.pipe(
      filter(event => event instanceof NavigationEnd)
    ).subscribe((event: NavigationEnd) => {
-     this.onHomePage = event.urlAfterRedirects === '/home';
+    this.ngZone.run(() => {
+      this.onHomePage = event.urlAfterRedirects === '/home' || event.urlAfterRedirects === '/about';
+
+    });
    });
+ }
+
+ logout(){
+  this.loginService.logout();
  }
  }
